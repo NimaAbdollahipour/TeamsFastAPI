f1d7f6678e73914ac0fbcc32a3c052dc4e37b521
Nima Abdollahipour <nima.abdollahi.a@gmail.com>
2023-12-21 19:55:49 +0200

    cors error

--- teams/__init__.py
+++ teams/__init__.py
@@ -1,6 +1,7 @@
 from .database import session
 from .admin_api import admin_router
 from .auth2 import auth_router
+from fastapi.middleware.cors import CORSMiddleware
 from .general_api import general_router
 from .teacher_api import teacher_router
 from .student_api import student_router
@@ -17,6 +18,16 @@ from .models import (
 from fastapi import FastAPI, APIRouter
 
 app = FastAPI()
+
+origins = []
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(admin_router)
 app.include_router(auth_router)
 app.include_router(general_router)
